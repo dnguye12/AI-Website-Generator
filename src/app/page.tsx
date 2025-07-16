30b65f7522b44d86c28731526214a82dea6e5bd5
dnguye12 <duchuyng051@gmail.com>
2025-07-16 10:14:52 +0200

    openai init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+"use client"
+
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
 const Page = () => {
+  const [value, setValue] = useState("")
 
   return (
-    <></>
+    <div className=" p-4 max-w-7xl mx-auto">
+      <Input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </div>
   );
 }
 
